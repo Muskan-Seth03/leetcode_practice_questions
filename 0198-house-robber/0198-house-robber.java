2741b70b274f76695fe3e1c835ea53c8f7186f7d
Muskan Seth <muskanseth003@gmail.com>
2025-02-14 14:22:07 +0530

    Time: 0 ms (100%), Space: 41.1 MB (59.3%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,25 +1,15 @@
-class Solution {   //Memoization   TC: O(2^n)*O(1) 
-    public int f(int[] nums, int i, int dp[])
-    {
-        if(i>= nums.length)
-        return 0;
-        if(i== nums.length -1)
-        return nums[nums.length-1];
-
-        if(dp[i]!=-1)
-        return dp[i];
-
-        int rob= nums[i] + f(nums, i+2, dp);
-        int not_rob= f(nums, i+1, dp);
-        dp[i]= Math.max(rob, not_rob);
-        return dp[i];
-    }
+class Solution { //Tabulation   TC: O(2^n)*O(1)
 
     public int rob(int[] nums) {
-        int n= nums.length;
-        int dp[]= new int [n];
-        Arrays.fill(dp, -1);
-        
-        return f(nums, 0, dp);
+        int n = nums.length;
+        int dp[] = new int[n + 1];
+        dp[n] = 0;
+        dp[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            int rob = nums[i] + dp[i + 2];
+            int not_rob =dp[i + 1];
+            dp[i] = Math.max(rob, not_rob);
+        }
+        return dp[0];
     }
 }
