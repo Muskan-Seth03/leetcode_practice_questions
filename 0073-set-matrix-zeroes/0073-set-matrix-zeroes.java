7174c74f0d76a057ac1afe594a745687200bd03b
Muskan Seth <muskanseth003@gmail.com>
2025-02-23 11:32:27 +0530

    Time: 1 ms (75.44%), Space: 45.6 MB (65.38%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,34 +1,35 @@
-class Solution {
+//mark the indices in row[i] arr and cols[j] arr for {i, j} wherever we encounter 0   
+class Solution {  //better approach using 2 array            
     public void setZeroes(int[][] matrix) {
         int rows= matrix.length;
         int cols= matrix[0].length;
-        
-        Queue<int[]> queue = new LinkedList<>();
-        // Store initial position of all zeroes in the matrix
-        for(int i=0; i< rows; i++)
-        {
+
+        int row_array[]= new int [rows];
+        int cols_array[]= new int [cols];
+       
+       for(int i=0; i<rows; i++)
+       {
             for(int j=0; j<cols; j++)
             {
                 if(matrix[i][j]==0)
-                queue.add(new int[]{i, j});
+                {
+                    // mark the ith index of row_array to 1
+                    row_array[i]=1;
+                    cols_array[j]=1;
+                }
             }
         }
-
-        while(!queue.isEmpty())
-        {
-            int [] cell= queue.poll();
-            int r = cell[0];
-            int c = cell[1];
-            for(int j=0; j<cols; j++)  // in a particular row
+        for(int i=0; i<rows; i++)
+       {
+            for(int j=0; j<cols; j++)
             {
-                matrix[r][j]=0;
-            }
-            for(int i=0; i< rows; i++)  // in a particular col
-            {
-                matrix[i][c]=0;
-            }
+                if(row_array[i]==1 ||  cols_array[j]==1)
+                {
+                    matrix[i][j]=0;
+                }
 
+            }
         }
-
+        
     }
 }
