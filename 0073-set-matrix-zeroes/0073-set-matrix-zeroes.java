b985aa1c3b2e9d4cb65b1ede478715a7ff9b042d
Muskan Seth <muskanseth003@gmail.com>
2025-02-25 12:08:43 +0530

    Time: 1 ms (75.5%), Space: 45.8 MB (28.84%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,35 +1,45 @@
-//mark the indices in row[i] arr and cols[j] arr for {i, j} wherever we encounter 0   
-class Solution {  //better approach using 2 array            
+class Solution { // optimal approach TC:O(2*M*N) SC: O(1)
+
     public void setZeroes(int[][] matrix) {
-        int rows= matrix.length;
-        int cols= matrix[0].length;
+        int col0 = 1;
 
-        int row_array[]= new int [rows];
-        int cols_array[]= new int [cols];
-       
-       for(int i=0; i<rows; i++)
-       {
-            for(int j=0; j<cols; j++)
-            {
-                if(matrix[i][j]==0)
-                {
-                    // mark the ith index of row_array to 1
-                    row_array[i]=1;
-                    cols_array[j]=1;
+        // set 0 in the 1 st row and 1 st cols if its corresponding cell contains 0
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    // mark the i th row
+                    matrix[i][0] = 0;
+
+                    // mark the j th col
+                    if (j != 0) {
+                        matrix[0][j] = 0;
+                    } else col0 = 0;
                 }
             }
         }
-        for(int i=0; i<rows; i++)
-       {
-            for(int j=0; j<cols; j++)
-            {
-                if(row_array[i]==1 ||  cols_array[j]==1)
-                {
-                    matrix[i][j]=0;
-                }
 
+        //convert number to 0 if either 1 st row or 1 st cols is marked as zero
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][j] != 0) {
+                    if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+                        matrix[i][j] = 0;
+                    }
+                }
             }
         }
-        
+        // finally mark the 1st col and then 1 st row  to get the correct answer
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // mark the 1 st row
+        if (col0 == 0) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
     }
 }
