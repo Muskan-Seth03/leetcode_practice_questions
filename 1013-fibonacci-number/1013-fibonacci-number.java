6ba10852061edfe379a73c986709fba89c091b77
Muskan Seth <muskanseth003@gmail.com>
2025-02-28 21:47:27 +0530

    Time: 203 ms (11.76%), Space: 44.6 MB (6.07%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,9 +1,23 @@
-class Solution { //recursion TC: O(2^n) SC:O(n)
+class Solution { // memoization (top-down)TC: O() SC:O()
     public int fib(int n) {
+        int dp[]= new int[31];  
+        Arrays.fill(dp, -1);
+        
         if(n<=1)
         return n;
+
+        int first, second;
+        if(dp[n-1]!=-1)
+        first= dp[n-1];
         else
-        return fib(n-1) + fib(n-2);
+        first= fib(n-1);
+
+        if(dp[n-2]!=-1)
+        second= dp[n-2];
+        else
+        second= fib(n-2);
+
+        return dp[n]= first + second;
         
     }
 }
