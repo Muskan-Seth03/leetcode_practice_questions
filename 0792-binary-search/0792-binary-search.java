daa03ed842476f915a62e0088407f70aa44ca7aa
Muskan Seth <muskanseth003@gmail.com>
2025-03-01 16:49:13 +0530

    Time: 0 ms (100%), Space: 45.6 MB (88.61%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,25 +1,19 @@
-class Solution { // recursion
-
-    public int binarySearch(int[] nums, int target, int low, int high) {
-        if(low > high)
-        return -1;      
-
-        int mid = (low + high) / 2;
-
-
-        if (nums[mid] == target) return mid;
-
-        else if (nums[mid] < target) {
-            return binarySearch(nums, target, mid + 1, high);
-        }
-        else {
-            return binarySearch(nums, target, low, mid - 1);
-        }
-        
-    }
+class Solution {
 
     public int search(int[] nums, int target) {
-        int n= nums.length;
-        return binarySearch(nums, target, 0, n - 1);
+        int n = nums.length;
+        int left = 0;
+        int right = n - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
     }
 }
