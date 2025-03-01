21adffe3deda04a1b861bea61f5ea995a7dfc954
Muskan Seth <muskanseth003@gmail.com>
2025-03-01 11:47:30 +0530

    Time: 0 ms (100%), Space: 43.1 MB (61.69%) - LeetHub

---
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution { // Tabulation TC:O(n) SC:O(n)
+
+    public int solve(int [] cost, int n){
+        int[] dp= new int[n+1];
+        //base case
+        dp[0]= cost[0];
+        dp[1]= cost[1];
+
+        for(int i=2; i<n; i++)
+        {
+            dp[i]= cost[i]+ Math.min(dp[i-1], dp[i-2]);
+        }
+        return Math.min(dp[n-1], dp[n-2]);
+    }
+
+    public int minCostClimbingStairs(int[] cost) {
+        int n= cost.length;
+
+        return solve(cost, n);
+        
+    }
+}
