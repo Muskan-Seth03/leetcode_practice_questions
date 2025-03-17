161bb136614e21c173e6a7bc6b0e82472781b135
Muskan Seth <muskanseth003@gmail.com>
2025-03-17 10:06:00 +0530

    Time: 10 ms (24.16%), Space: 46.5 MB (83.54%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,25 +1,22 @@
-class Solution {
+class Solution {   //using stack 
     public int[][] merge(int[][] intervals) {
-        //sort the  2 d array
-        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
-        int [] prev=intervals[0];
+        // Sort the intervals based on the start value
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        Stack<int[]> s = new Stack<>();
 
-        List<int []> merged=new ArrayList<>();
-        
-        for(int i=1;i<intervals.length;i++)
-        {
-            int [] curr=intervals[i];
-            if(prev[1]<curr[0])
+        for (int[] interval : intervals) {
+            if (s.isEmpty() || s.peek()[1] < interval[0]) 
             {
-                merged.add(prev);
-                prev=curr;
-            }
-            else{
-                prev[1]=Math.max(curr[1],prev[1]);
+                // No overlap, push interval
+                s.push(interval);
+            } 
+            else 
+            {
+                // Merge intervals (update top element of stack)
+                s.peek()[1] = Math.max(s.peek()[1], interval[1]);
             }
         }
-        merged.add(prev);
- 
-        return merged.toArray(new int [merged.size()][]);
+
+        return s.toArray(new int[s.size()][]);
     }
 }
