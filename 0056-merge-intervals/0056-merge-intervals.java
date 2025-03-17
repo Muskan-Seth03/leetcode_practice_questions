3c9c4cfb726cc9c9876c5b8944df30312095c55d
Muskan Seth <muskanseth003@gmail.com>
2025-03-17 09:44:30 +0530

    Time: 8 ms (86.83%), Space: 46.6 MB (54.75%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,34 +1,25 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        if(intervals.length==0)
-        return new int [0][0];
+        //sort the  2 d array
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        int [] prev=intervals[0];
 
-        //sort the 2 d array
-        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
-        // create resultant list (list of array)
-        List<int[]> res= new ArrayList<>();
-        int [] curr= intervals[0];
-        res.add(curr);
-
-        for(int i=1; i<intervals.length; i++)
+        List<int []> merged=new ArrayList<>();
+        
+        for(int i=1;i<intervals.length;i++)
         {
-            int curr_end = curr[1];
-            int next_start= intervals[i][0];
-            int next_end= intervals[i][1];
-            // case of overlapping intervals
-            if(curr_end >= next_start)
+            int [] curr=intervals[i];
+            if(prev[1]<curr[0])
             {
-                curr[1]= Math.max(curr_end, next_end);
+                merged.add(prev);
+                prev=curr;
             }
-            //non-overlapping case  -- keep as it is  
-            else
-            {
-                curr= intervals[i];
-                res.add(curr);
+            else{
+                prev[1]=Math.max(curr[1],prev[1]);
             }
         }
-
-        return res.toArray(new int [res.size()][]);
-        
+        merged.add(prev);
+ 
+        return merged.toArray(new int [merged.size()][]);
     }
 }
