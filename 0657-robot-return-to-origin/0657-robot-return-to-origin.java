6336a58d6266a5e3c900cb0af8967107ff953212
Muskan Seth <muskanseth003@gmail.com>
2025-04-10 19:48:58 +0530

    Time: 5 ms (56.33%), Space: 44.8 MB (33.85%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -1,46 +1,12 @@
-class Solution {   // brute force approach  TC:O(n)  SC:O(n)
+class Solution {
     public boolean judgeCircle(String moves) {
-        char[] ch= moves.toCharArray();
-        int n= ch.length;
-        int row=0;
-        int col=0;
-        int dir_r=0;
-        int dir_c=0;
-        for(int i=0; i<n; i++)
-        {
-            if(ch[i]=='U')
-            {
-                dir_r=-1;
-                dir_c= 0;
-            }
-            else if(ch[i] == 'D')
-            {
-                dir_r= 1;
-                dir_c= 0;
-            }
-            else if(ch[i] == 'L')
-            {
-                dir_r= 0;
-                dir_c= -1;
-            }
-            else
-            {
-                dir_r= 0;
-                dir_c= 1;
-            }
-            row= row + dir_r;
-            col= col + dir_c;
-
+        int x = 0, y = 0;
+        for (char c : moves.toCharArray()) {
+            if (c == 'R') x++;
+            if (c == 'L') x--;
+            if (c == 'U') y++;
+            if (c == 'D') y--;
         }
-
-        if(row == 0 && col==0)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-        
+        return x == 0 && y == 0;
     }
 }
