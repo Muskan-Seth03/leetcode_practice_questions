2c387a449156b411153dbf3e7b248f7dc513d0c3
Muskan Seth <muskanseth003@gmail.com>
2025-04-11 21:59:18 +0530

    Time: 6 ms (25.16%), Space: 52.7 MB (10.6%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,24 +1,69 @@
+class Pair   // using bfs to keep track of starting node and visi array to keep track of nodes visited 
+{
+    int first;
+    int second;
+    Pair(int first, int second)
+    {
+        this.first= first;
+        this.second= second;
+    }
+    
+}
 class Solution {
+    public static void  bfs(int i, int j, int [][]visi, char[][]grid)
+    {
+        int m= grid.length;
+        int n= grid[0].length;
+        visi[i][j]=1;
+        Queue<Pair> q= new LinkedList<Pair>();
+        q.offer(new Pair(i, j));
+
+        while(!q.isEmpty())
+        {
+            Pair it= q.poll();
+            int row= it.first;
+            int col= it.second;
+
+            int dir_r[] = {-1, 0, 1, 0};
+            int dir_c[]= {0, 1, 0, -1};
+            for(int k=0; k<4; k++)
+            {
+                int new_row= row + dir_r[k];
+                int new_col= col + dir_c[k];
+
+    if(new_row>=0 && new_row<m && new_col>=0 && new_col<n && grid[new_row][new_col] =='1' && visi[new_row][new_col]==0)
+                {
+                    visi[new_row][new_col]=1;
+                    q.offer(new Pair(new_row, new_col));
+                }
+            }
+        }
+    }
+
+
     public int numIslands(char[][] grid) {
-        if(grid == null || grid.length == 0) return 0;
-        int count = 0;
-        for(int i=0; i<grid.length; i++){
-            for(int j=0; j<grid[0].length; j++){
-                if(grid[i][j] == '1'){
+        int m= grid.length;
+        int n= grid[0].length;
+        int visi[][]= new int[m][n];
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                visi[i][j]= 0;
+            }
+        }
+        int count=0;
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(visi[i][j]==0 && grid[i][j]=='1')
+                {
                     count++;
-                    dfs(grid, i, j);
+                    bfs(i, j, visi, grid);
                 }
             }
         }
         return count;
-    }
-    
-    private static void dfs(char[][] grid, int i, int j) {
-        if(i<0 || i>=grid.length || j <0 || j>=grid[0].length || grid[i][j] != '1') return;
-        grid[i][j] = '0';
-        dfs(grid, i+1, j);
-        dfs(grid, i-1, j);
-        dfs(grid, i, j+1);
-        dfs(grid, i, j-1);
     }
 }
