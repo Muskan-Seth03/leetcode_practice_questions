85241da53ecf2e643929536a1e76b2f4ee5bd979
Muskan Seth <muskanseth003@gmail.com>
2025-04-13 12:23:12 +0530

    Time: 3 ms (31.03%), Space: 45.3 MB (12.91%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,15 +1,26 @@
-class Solution {  // iterative dfs  TC:O(m*n)  SC:O(m*n)
+class Pair{     // using bfs  TC:O(m*n)  SC:O(m*n)
+    int first;
+    int second;
+    Pair(int first, int second)
+    {
+        this.first= first;
+        this.second= second;
+    }
+}
+class Solution {
     public int maxAreaOfIsland(int[][] grid) {
         if (grid == null || grid.length == 0) return 0;
         int m = grid.length, n = grid[0].length;
         int max_area=0;
-        int count = 0;
+        int count=0;
+
+        int visi[][]= new int [m][n];
 
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 1) {
+                if (visi[i][j] == 0 && grid[i][j]==1) {
                     count++;
-                    int area=dfs(grid, i, j);
+                    int area=bfs(grid, i, j, visi);
                     max_area= Math.max(max_area, area);
                 }
             }
@@ -20,48 +31,42 @@ class Solution {  // iterative dfs  TC:O(m*n)  SC:O(m*n)
             return 0;
         }
         return max_area;
-        
     }
 
-    private static int dfs(int[][] grid, int i, int j) {
+    private static int bfs(int[][] grid, int i, int j, int [][]visi) {
         int m = grid.length, n = grid[0].length;
-        // boundary and water check 
-        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0) {
-            return 0;
-        }
-
-        Stack<int[]> s= new Stack<>();
-        s.push(new int[]{i, j});
-
-        grid[i][j] = 0;   //mark it as visited
         
+        Queue<Pair> q= new LinkedList<>();
+        q.offer(new Pair(i, j));
+        visi[i][j]=1;
+
+        int area=1;
         int dir_r[]= {0, -1, 0, 1};
         int dir_c[]= {-1, 0, 1, 0};
         
-        int area=1;
-        while(!s.isEmpty())
+        while(!q.isEmpty())
         {
-            int [] it= s.pop();
-            int row= it[0];
-            int col= it[1];
-
+            Pair it= q.poll();
+            int row= it.first;
+            int col= it.second;
+            
             for(int k=0; k<4; k++)
             {
                 int new_r= row + dir_r[k];
                 int new_c= col + dir_c[k];
 
-                if( new_r>=0 && new_r <m && new_c>=0 && new_c<n && grid[new_r][new_c]== 1)
+                if( new_r>=0 && new_r <m && new_c>=0 && new_c<n && grid[new_r][new_c]==1 && visi[new_r][new_c]== 0)
                 {
                     area++;
-                    s.push(new int []{new_r, new_c});
-                    // mark neighbour as vsited
-                    grid[new_r][new_c]=0;
+                    q.offer(new Pair(new_r, new_c));
+                    visi[new_r][new_c]=1;
                 }
 
             }
-
         }
-
         return area;
+
     }
 }
+
+            
