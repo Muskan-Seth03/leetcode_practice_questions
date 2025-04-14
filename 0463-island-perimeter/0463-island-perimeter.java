f175b1be1b2acc4ec696ebc74223c2eb219525fa
Muskan Seth <muskanseth003@gmail.com>
2025-04-14 21:15:31 +0530

    Time: 7 ms (27.45%), Space: 46.1 MB (24.26%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,4 +1,4 @@
-class Solution {   // iterative dfs   TC:O(m*n)  SC:O(m*n)
+class Solution {   // recursive dfs   TC:O(m*n)  SC:O(m*n)
     public int islandPerimeter(int[][] grid) {
         if (grid == null || grid.length == 0) return 0;
         int m = grid.length, n = grid[0].length;
@@ -8,59 +8,32 @@ class Solution {   // iterative dfs   TC:O(m*n)  SC:O(m*n)
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == 1) {
-                    count++;
-                    perimeter=dfs(grid, i, j);
+                    perimeter= dfs(grid, i, j);
                 }
             }
         }
-
-        if(count==0)  // no island present
-        {
-            return 0;
-        }
         return perimeter;
-        
     }
 
     private static int dfs(int[][] grid, int i, int j) {
         int m = grid.length, n = grid[0].length;
         int perimeter=0;
 
-        Stack<int[]> s= new Stack<>();
-        s.push(new int[]{i, j});
-        
-        int dir_r[]= {0, -1, 0, 1};
-        int dir_c[]= {-1, 0, 1, 0};
-        
-        while(!s.isEmpty())
-        {
-            int [] it= s.pop();
-            int row= it[0];
-            int col= it[1];
-        
-            grid[row][col] = -1; // Mark as visited
-
-            for(int k=0; k<4; k++)
-            {
-                int new_r= row + dir_r[k];
-                int new_c= col + dir_c[k];
-
-                if( new_r>=0 && new_r <m && new_c>=0 && new_c<n && grid[new_r][new_c]== 1)
-                {
-                    s.push(new int []{new_r, new_c});
-                    grid[new_r][new_c]=-1;
-                }
-                
-                // boundary and water check 
-  else if (new_r < 0 || new_r >= m || new_c < 0 || new_c >= n || grid[new_r] [new_c] == 0) {
-                    perimeter++;
-                
-                }
-
-            }
-
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i] [j] == 0) {
+            return  1;
         }
 
-        return perimeter;
-    }
+        if(grid[i][j]==-1)
+        return 0;   // already visited , no perimeter contribution
+
+        grid[i][j]=-1;
+
+        perimeter+=dfs(grid, i, j-1);
+        perimeter+=dfs(grid, i-1, j);
+        perimeter+=dfs(grid, i, j+1);
+        perimeter+=dfs(grid, i+1, j);
+
+         return perimeter;
 }
+    }
+   
