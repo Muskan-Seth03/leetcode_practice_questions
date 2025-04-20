c4f3d817415d7e52e431928db9ce105081a2d977
Muskan Seth <muskanseth003@gmail.com>
2025-04-20 12:13:52 +0530

    Time: 20 ms (75.06%), Space: 83.9 MB (49.36%) - LeetHub

--- 2035-count-sub-islands/2035-count-sub-islands.java
+++ 2035-count-sub-islands/2035-count-sub-islands.java
@@ -1,41 +1,27 @@
-class Solution { //dfs + stack (no visi array --in place change of visited land cell to water )
-                //TC:O(m*n)   SC:O(m*n) 
-
-    public static int dfs(int row, int col, int [][]grid1, int[][]grid2)
+class Solution { // recursive dfs + in-place grid2 marking
+                 // TC: O(m * n), SC: O(m * n) (recursion stack)
+    public static int dfs(int r, int c, int [][]grid1, int[][]grid2)
     {
         int m= grid1.length;
         int n= grid1[0].length;
-        Stack<int[]> s= new Stack<>();
 
-        int count=1;
-        s.push(new int[]{row, col});
+        if(r<0 || r>=m || c<0 || c>=n || grid2[r][c]==0)
+        return 1;
         
-        int dr[]={-1,0,1,0};
-        int dc[]= {0,1,0,-1};
-        
-        while(!s.isEmpty())
-        {
-            int [] it= s.pop();
-            int r= it[0];
-            int c= it[1];
+        // Mark as visited in grid2
+        grid2[r][c] = 0;
 
-            if(grid1[r][c]!=1) 
-            count=0;
+        // Check current cell validity in grid1
+        int isValid = (grid1[r][c] == 1) ? 1 : 0;
 
-            for(int i=0; i<4; i++)
-            {
-                int new_r= r+ dr[i];
-                int new_c= c + dc[i];
+        // Explore 4 directions
+        int up = dfs(r - 1, c, grid1, grid2);
+        int down = dfs(r + 1, c, grid1, grid2);
+        int left = dfs(r, c - 1, grid1, grid2);
+        int right = dfs(r, c + 1, grid1, grid2);
 
-                if(new_r>=0 && new_r<m && new_c>=0 && new_c< n && grid2[new_r][new_c]==1)
-                {
-                    grid2[new_r][new_c]=0;  // visit that cell 
-                    s.push(new int[]{new_r, new_c});
-                }
-            }
-        }
-
-        return count;
+        // All neighbors must also be valid sub-island
+        return isValid & up & down & left & right;
     }
                 
     public int countSubIslands(int[][] grid1, int[][] grid2) {
@@ -49,7 +35,6 @@ class Solution { //dfs + stack (no visi array --in place change of visited land 
             {
                 if(grid2[i][j]==1)  // land cell
                 {
-                    grid2[i][j]=0;
                     count+= dfs(i, j, grid1, grid2);  
                 }
             }
