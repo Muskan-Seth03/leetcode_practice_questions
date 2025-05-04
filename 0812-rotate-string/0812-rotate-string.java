1fb697489673974b9629c878cd9ac8ee7938c1a6
Muskan Seth <muskanseth003@gmail.com>
2025-05-04 23:14:34 +0530

    Time: 0 ms (100%), Space: 41.3 MB (90.67%) - LeetHub

---
+++ 0812-rotate-string/0812-rotate-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length()!= goal.length())
+        return false;
+
+        String concatenated= goal.concat(goal);
+
+        return concatenated.contains(s);
+        
+    }
+}
