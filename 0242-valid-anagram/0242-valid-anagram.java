64ee93aa3a67573f7010a970e6ffaf27e123ee9c
Muskan Seth <muskanseth003@gmail.com>
2025-05-05 00:44:04 +0530

    Time: 3 ms (90.49%), Space: 44.7 MB (44.42%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,29 +1,13 @@
-class Solution {  //TC: O(n)  SC:O(1)
+import java.util.Arrays;   //sorting 
+
+class Solution {
     public boolean isAnagram(String s, String t) {
-        if( s.length() != t.length())
-        return false;
-
-        HashMap<Character, Integer> map= new HashMap<>();
-        for(int i=0; i< s.length(); i++)
-        {
-            char ch= s.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-        }
-
-        for(char ch:t.toCharArray())
-        {
-            if(map.containsKey(ch))
-            {
-                map.put(ch, map.get(ch)-1);
-            }
-        }
-        // Check if any character has non-zero frequency
-        for (int val : map.values()) {
-            if (val != 0) {
-                return false;
-            }
-        }
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
         
-        return true;
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        
+        return Arrays.equals(sChars, tChars);
     }
 }
