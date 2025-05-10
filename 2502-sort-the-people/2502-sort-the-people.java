e0eb4df5835f9813a657cc4e43e059a2e390578f
Muskan Seth <muskanseth003@gmail.com>
2025-05-10 11:36:53 +0530

    Time: 7 ms (94.02%), Space: 45.4 MB (62.12%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.java
+++ 2502-sort-the-people/2502-sort-the-people.java
@@ -1,41 +1,19 @@
-//  Object modelling apporach  
-// TC: O(n log n)   sorting  the array
-// SC: O(n)   allocating space for n Person objects → O(n)
-
-
 class Solution {
-    static class Person
-    {
-        String name;
-        int height;
-        Person(String name, int height)
-        {
-            this.name= name;
-            this.height= height;
-        }
-    }
-
     public String[] sortPeople(String[] names, int[] heights) {
-        int n= names.length;
-
-//creates an array of Person objects with size n, but does not yet fill it with actual objects.
-        Person people[] = new Person [n]; 
-        for(int i=0; i< n; i++)
+        Map<Integer,String> map= new HashMap<>();
+        for(int i=0;i<names.length;i++)
         {
-            people[i]= new Person(names[i], heights[i]);
-        } 
-        // sort the people array in descending order
-        Arrays.sort(people, (a, b) -> b.height - a.height); 
-
-        String result[]= new String[n];
-        for(int i=0; i<n ; i++)
-        {
-            result[i]= people[i].name;
+            map.put(heights[i],names[i]);
         }
 
-        return result;
+        Arrays.sort(heights);
 
+        String[] sortedNames= new String[names.length];
+        int index=0;
+        for(int i=heights.length-1;i>=0;i--)
+        {
+            sortedNames[index++]=map.get(heights[i]);
+        }
+        return sortedNames;
     }
 }
-
-      
