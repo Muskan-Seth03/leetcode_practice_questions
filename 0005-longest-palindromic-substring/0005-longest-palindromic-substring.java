e87e32f5c76674f4c1c88c0a1496e58461d24123
Muskan Seth <muskanseth003@gmail.com>
2025-05-16 18:20:33 +0530

    Time: 16 ms (67.11%), Space: 42.4 MB (73.23%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,37 +1,37 @@
-class Solution {  //  brute force  TC:O(n^3)  SC:O(1)
-    public boolean isPalindrome(String s, int start, int end)
-    {
-        while(start<=end)
-        {
-            if(s.charAt(start) == s.charAt(end))
-            {
-                start++;
-                end--;
-            }
-            else
-            return false;
-        }
-        return true;
-    }
+class Solution {   // TC:O(n^2) SC:O(1)
     public String longestPalindrome(String s) {
-        int n= s.length();
-        int max_len=0;
-        String str1= "";
-        for(int i=0; i<n; i++)
-        {
-            for(int j=i; j<n; j++)
-            {
-                if(isPalindrome(s, i, j))
-                {
-                    if(j+1-i > max_len)
-                    {
-                        max_len= j+1-i;
-                        str1= s.substring(i, j+1);
+        int n = s.length();
+        if (n <= 1) return s;
 
-                    }
+        int start = 0;
+        int maxLen = 1;
+
+        for (int i = 1; i < n; i++) {
+            // Even length palindrome
+            int l = i - 1;
+            int r = i;
+            while (l >= 0 && r < n && s.charAt(l) == s.charAt(r)) {
+                if (r - l + 1 > maxLen) {
+                    maxLen = r - l + 1;
+                    start = l;
                 }
+                l--;
+                r++;
+            }
+
+            // Odd length palindrome
+            l = i - 1;
+            r = i + 1;
+            while (l >= 0 && r < n && s.charAt(l) == s.charAt(r)) {
+                if (r - l + 1 > maxLen) {
+                    maxLen = r - l + 1;
+                    start = l;
+                }
+                l--;
+                r++;
             }
         }
-        return str1;
+
+        return s.substring(start, start + maxLen);
     }
 }
