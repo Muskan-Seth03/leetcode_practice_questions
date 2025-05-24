a07098be2774bae05b7763214cfa844e61b6766f
Muskan Seth <muskanseth003@gmail.com>
2025-05-24 12:40:19 +0530

    Time: 4 ms (99.21%), Space: 44.9 MB (88.74%) - LeetHub

--- 2836-neither-minimum-nor-maximum/2836-neither-minimum-nor-maximum.java
+++ 2836-neither-minimum-nor-maximum/2836-neither-minimum-nor-maximum.java
@@ -1,11 +1,17 @@
-class Solution {   // brute force approach  TC:O(n) SC:O(1)
+//  TC:O(n) SC:O(1)
+// mirco - optimization approach without using Arrays.stream()
+class Solution { 
     public int findNonMinOrMax(int[] nums) {
         int n= nums.length;
         if(n < 3)
         return -1;
         
-        int max= Arrays.stream(nums).max().getAsInt();
-        int min= Arrays.stream(nums).min().getAsInt();
+        int min = nums[0], max = nums[0];
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+
         for(int num: nums)
         {
             if(num != max && num != min)
