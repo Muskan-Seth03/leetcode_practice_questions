5e357994f6d6a75832a6bacb2b2f2d11df588ade
Muskan Seth <muskanseth003@gmail.com>
2025-06-03 13:25:09 +0530

    Time: 23 ms (38.99%), Space: 51.3 MB (20.98%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -1,22 +1,29 @@
-class Solution {  //using dp  TC:O(m*n) SC:O(m*n)
+class Solution {
+    public int f(String text1, String text2, int ind1, int ind2, int dp[][])
+    {
+        if(ind1 < 0 || ind2 <0)
+        return 0;
+
+        if(dp[ind1][ind2] !=-1)
+        return dp[ind1][ind2];
+
+        if(text1.charAt(ind1) == text2.charAt(ind2))   // if match found
+        return dp[ind1][ind2] = 1 + f(text1, text2, ind1 - 1, ind2 - 1, dp);
+
+        else
+        return dp[ind1][ind2] = 0 + Math.max(f(text1, text2, ind1 -1, ind2, dp), f(text1, text2, ind1, ind2 -1, dp)); 
+
+    }
+
     public int longestCommonSubsequence(String text1, String text2) {
-        int m = text1.length();
-        int n = text2.length();
+        int n= text1.length();
+        int m= text2.length();
+        int dp[][]= new int[n][m];
+        
+        for(int row[]: dp)
+        Arrays.fill(row, -1);
 
-        // Create a 2D DP array
-        int[][] dp = new int[m + 1][n + 1];
+        return f(text1, text2, n-1, m-1, dp);
 
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                // If characters match, take diagonal + 1
-                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                } else {
-                    // Else, take max from top or left cell
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
-        }
-        return dp[m][n];
     }
 }
