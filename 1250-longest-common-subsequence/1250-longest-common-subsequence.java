dd8384244aace538ee52537f21e27ee2cf83edb4
Muskan Seth <muskanseth003@gmail.com>
2025-06-04 10:37:37 +0530

    Time: 31 ms (12.4%), Space: 50.8 MB (67.2%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -1,17 +1,19 @@
-class Solution {
+class Solution {  // memoization approach   TC:O(m*n)  SC:O(N*M) + O(N+M)
     public int f(String text1, String text2, int ind1, int ind2, int dp[][])
     {
-        if(ind1 < 0 || ind2 <0)
+        int n= text1.length();
+        int m= text2.length();
+        if(ind1 >=n || ind2 >=m)
         return 0;
 
         if(dp[ind1][ind2] !=-1)
         return dp[ind1][ind2];
 
         if(text1.charAt(ind1) == text2.charAt(ind2))   // if match found
-        return dp[ind1][ind2] = 1 + f(text1, text2, ind1 - 1, ind2 - 1, dp);
+        return dp[ind1][ind2] = 1 + f(text1, text2, ind1 + 1, ind2 + 1, dp);
 
         else
-        return dp[ind1][ind2] = 0 + Math.max(f(text1, text2, ind1 -1, ind2, dp), f(text1, text2, ind1, ind2 -1, dp)); 
+        return dp[ind1][ind2] = 0 + Math.max(f(text1, text2, ind1 +1, ind2, dp), f(text1, text2, ind1, ind2 + 1, dp)); 
 
     }
 
@@ -23,7 +25,7 @@ class Solution {
         for(int row[]: dp)
         Arrays.fill(row, -1);
 
-        return f(text1, text2, n-1, m-1, dp);
+        return f(text1, text2, 0, 0, dp);
 
     }
 }
