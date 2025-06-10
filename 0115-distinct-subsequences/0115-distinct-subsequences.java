3f0e96ff9e2c64066d5566dc4c86c55b7cdf4b44
Muskan Seth <muskanseth003@gmail.com>
2025-06-10 14:04:17 +0530

    Time: 20 ms (43.93%), Space: 51.3 MB (55.93%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,24 +1,28 @@
-class Solution {  // memoization approach TC:O(n*m)   SC:O(n*m) + O(n+m)
-    public static int f(String s, String t, int i, int j, int [][]dp)
-    {   
-        //base case
-        if(j< 0) return 1;
-        if(i< 0) return 0;
-
-        if(dp[i][j]!=-1)
-        return dp[i][j];
-        
-        if(s.charAt(i) == t.charAt(j))
-        return dp[i][j]= f(s, t, i-1, j-1, dp) + f(s, t, i-1, j, dp);
-        else
-        return dp[i][j]= f(s, t, i-1, j, dp);
-    } 
+class Solution {  // tabulation approach TC:O(n*m)   SC:O(n*m)
     public int numDistinct(String s, String t) {
         int n= s.length();
         int m= t.length();
-        int dp[][]= new int [n][m];
+        int dp[][]= new int [n+1][m+1];
         for(int row[]: dp)
-        Arrays.fill(row, -1);
-        return f(s, t , n-1, m-1, dp);
+        Arrays.fill(row, 0);
+
+        //base case
+        for(int i=0; i<=n; i++) dp[i][0]=1;
+        for(int j=1; j<=m; j++) dp[0][j]=0;
+
+
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=m; j++)
+            {
+                if(s.charAt(i-1) == t.charAt(j-1))
+                {
+                    dp[i][j]= dp[i-1][j-1] + dp[i-1][j]; 
+                }
+                else
+                dp[i][j]= dp[i-1][j]; 
+            }
+        }
+        return dp[n][m];
     }
 }
