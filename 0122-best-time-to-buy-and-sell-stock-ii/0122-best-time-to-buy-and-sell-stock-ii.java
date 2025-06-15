7d8c65ee7bdc1bc442de328d629a5fb1bcc65eb5
Muskan Seth <muskanseth003@gmail.com>
2025-06-15 10:45:52 +0530

    Time: 3 ms (11.56%), Space: 49.7 MB (5.17%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,16 +1,30 @@
-class Solution {
-    public int maxProfit(int[] prices) {
-        int total_profit = 0;
+class Solution {   // memoization approach  TC:O(n*2)  SC:O(n*2) + O(n) 
+    public static int f(int ind, int buy, int[]prices, int[][]dp)
+    {
+        int n= prices.length;
+        //base case
+        if(ind == n)
+        return 0;
 
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] > prices[i - 1]) {
-                total_profit += prices[i] - prices[i - 1]; 
-            }
-            else
-            {
-                total_profit+=0;
-            }
-        }
-        return total_profit;
+        if(dp[ind][buy]!= -1)
+        return dp[ind][buy];
+
+        int profit=0;
+        if(buy==1)  //allowed to buy 
+            profit= Math.max(-prices[ind] + f(ind+1, 0, prices, dp), 0 + f(ind+1, 1, prices, dp)); 
+        
+        else
+            profit= Math.max(prices[ind] + f(ind+1, 1, prices, dp), 0 + f(ind+1, 0, prices, dp));
+        return dp[ind][buy]= profit;
+    }
+
+    public int maxProfit(int[] prices) {
+        int n= prices.length;
+        int dp[][]= new int [n][2];
+
+        for(int [] row: dp)
+        Arrays.fill(row, -1);
+
+        return f(0, 1, prices, dp);
     }
 }
