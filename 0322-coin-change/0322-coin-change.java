2ab1fd4908a4f56ce986859df8fbda559bc1b69c
Muskan Seth <muskanseth003@gmail.com>
2025-06-21 01:14:32 +0530

    Time: 111 ms (6.9%), Space: 45.7 MB (13.04%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,27 +1,26 @@
-class Solution {   // optimization approach 
+class Solution {   // tabulation approach 
     public int coinChange(int[] coins, int amount) {
         int n= coins.length;
-        int[]prev= new int [amount + 1];
-        int[]curr= new int [amount + 1];
-
+        int[][]dp= new int [n][amount + 1];
         //base case 
         for (int i = 0; i <= amount; i++) {
         if (i % coins[0] == 0)
-            prev[i] = i / coins[0];
+            dp[0][i] = i / coins[0];
         else
-            prev[i] = (int) Math.pow(10, 9);
+            dp[0][i] = (int) Math.pow(10, 9);
         }
         for (int ind = 1; ind < n; ind++) {
             for (int target = 0; target <= amount; target++) {
-                int notTake = 0 + prev[target];
+                int notTake = 0 + dp[ind - 1][target];
                 int take = (int) Math.pow(10, 9);
+
                 if (coins[ind] <= target)
-                    take = 1 + curr[target - coins[ind]];
-                curr[target] = Math.min(notTake, take);
+                    take = 1 + dp[ind][target - coins[ind]];
+            
+                dp[ind][target] = Math.min(notTake, take);
             }
-            prev= curr.clone();
         }
-        int ans = prev[amount];
+        int ans = dp[n - 1][amount];
 
         if (ans >= (int)(1e9))
             return -1;
