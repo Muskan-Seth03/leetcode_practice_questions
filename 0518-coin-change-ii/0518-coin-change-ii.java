dd9f679b9c9c6c49dd1b95038815859363d60494
Muskan Seth <muskanseth003@gmail.com>
2025-06-22 11:51:34 +0530

    Time: 11 ms (37.41%), Space: 48.9 MB (54.22%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,24 +1,33 @@
-class Solution {  // tabulation approach
+class Solution {  // memoization approach
+    public static int f(int ind, int target, int[]coins, int[][]dp)
+    {
+        //base case 
+        if(ind==0)
+        {
+            if(target % coins[0] ==0)
+            return 1;
+            else
+            return 0;
+        }
+        if(dp[ind][target]!=-1)
+        return dp[ind][target];
+
+        int notTake = f(ind - 1, target, coins, dp);
+        int take = 0;
+
+        if (coins[ind] <= target)
+        take = f(ind, target - coins[ind], coins, dp);
+    
+        return dp[ind][target] = notTake + take;
+    }
     public int change(int amount, int[] coins) {
         int n= coins.length;
+        
         int[][]dp= new int [n][amount + 1];
-        //base case 
-        for (int i = 0; i <= amount; i++) 
-        {
-            if (i % coins[0] == 0)
-                dp[0][i] = 1;
-        }
-        for (int ind = 1; ind < n; ind++) {
-            for (int target = 0; target <= amount; target++) {
-                int notTake = dp[ind - 1][target];
-                int take = 0;
+        for(int row[]: dp)
+        Arrays.fill(row, -1);
 
-                if (coins[ind] <= target)
-                    take = dp[ind][target - coins[ind]];
-            
-                dp[ind][target] = notTake + take;
-            }
-        }
-        return  dp[n - 1][amount];
+        return f(n-1, amount, coins, dp);
     }
 }
+       
