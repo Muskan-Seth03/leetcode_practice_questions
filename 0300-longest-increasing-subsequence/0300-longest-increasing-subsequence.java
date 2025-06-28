c618e5e15e4acdb745d1b2adedddcc20ae5982a4
Muskan Seth <muskanseth003@gmail.com>
2025-06-28 10:20:19 +0530

    Time: 98 ms (19.52%), Space: 78.7 MB (12.42%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,28 +1,24 @@
-class Solution {   // memoization approach 
-    public static int f(int ind, int prev_index, int n, int[]nums, int[][]dp)
-    {
-        // base case
-        if(ind==n)
-        return 0;
-
-        if(dp[ind][prev_index+1]!=-1)
-        return dp[ind][prev_index + 1];
-
-        int not_take= 0 + f(ind + 1, prev_index, n, nums,dp);
-        int take= 0;
-        if(prev_index==-1 || nums[ind]> nums[prev_index])
-        {
-            // prev_index= ind;
-            take= 1 + f(ind+1, ind, n, nums,dp);
-        }
-        return dp[ind][prev_index+1]=Math.max(take, not_take);
-
-    }
+class Solution {   // tabulation approach 
     public int lengthOfLIS(int[] nums) {
         int n= nums.length;
-        int[][]dp= new int [n][n+1];
+        int[][]dp= new int [n+1][n+1];
         for(int []row:dp)
-        Arrays.fill(row, -1);
-        return f(0, -1, n, nums, dp);        
+        Arrays.fill(row, 0);
+
+        for(int ind= n-1; ind>=0; ind--)
+        {
+            for(int prev_index= ind-1; prev_index>=-1; prev_index--)
+            {
+                int not_take= 0 + dp[ind + 1][prev_index+ 1];
+                int take= 0;
+                if(prev_index==-1 || nums[ind]> nums[prev_index])
+                {
+                    // prev_index= ind;
+                    take= 1 + dp[ind+1][ind+1];
+                }
+                dp[ind][prev_index+1]=Math.max(take, not_take);
+            }
+        }
+        return dp[0][-1 + 1]; 
     }
 }
