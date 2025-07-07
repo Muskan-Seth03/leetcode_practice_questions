bd356a004dc8bc3ea55d58d39fab04afd13cad9e
Muskan Seth <muskanseth003@gmail.com>
2025-07-07 12:17:42 +0530

    Time: 1 ms (93.14%), Space: 44.4 MB (81.1%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,37 +1,38 @@
-// brute force
-// TC: O(n! * n)    ==> O(n!) for permutations + O(n) for putting ds into ans
-// SC: O(n! * n)    ==> O(n! *n) + O(n) recursive stack space + O(n) for map + O(n) for ds
+// space optimised approach
+// TC: O(n!* n)
+// SC: O(n) recursive stack space
 class Solution { 
-    public static void f(int[] nums, boolean map[], List<Integer> ds, List<List<Integer>> ans)
+    public static void f(int ind, int[] nums, List<List<Integer>> ans)
     {
         //base case
-        if(ds.size()==nums.length)
+        if(ind==nums.length)
         {
+            List<Integer> ds= new ArrayList<>();
+            for(int num: nums)
+            ds.add(num);
             ans.add(new ArrayList<>(ds));
             return;
         }
-        // pick only if position in map is not marked
-        for(int i= 0; i< map.length; i++)
+        // swap 
+        for(int i= ind; i< nums.length; i++)
         {
-            if(!map[i])
-            {
-                map[i]= true;
-                ds.add(nums[i]);
-
-                f(nums, map, ds, ans);
-                
-                map[i]= false;
-                ds.remove(ds.size()-1);
-            }
+            swap(i, ind, nums);          
+            f(ind+1, nums, ans);
+            swap(i, ind, nums);              
         }
+    }
+
+    public static void swap(int i, int j, int []nums)
+    {
+        int temp= nums[i];
+        nums[i]= nums[j];
+        nums[j]= temp;
     }
     public List<List<Integer>> permute(int[] nums) {
         int n= nums.length;
         List<List<Integer>> ans= new ArrayList<>();
-        List<Integer> ds= new ArrayList<>();
-        boolean [] map= new boolean[nums.length];
-
-        f(nums, map, ds, ans);
+      
+        f(0, nums, ans);
         return ans;
     }
 }
