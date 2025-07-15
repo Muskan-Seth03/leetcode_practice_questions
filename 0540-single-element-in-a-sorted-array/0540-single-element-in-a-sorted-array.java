24cc1e1afca7160a6c10a473b730bdf073457350
Muskan Seth <muskanseth003@gmail.com>
2025-07-15 09:22:42 +0530

    Time: 0 ms (100%), Space: 49.9 MB (96.66%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,29 +1,11 @@
-class Solution {  // brute force approach-1
+class Solution {  // brute force approach-2   XOR   TC: O(n)  SC: O(1) 
     public int singleNonDuplicate(int[] nums) {
         int n= nums.length;
-        // edge case
-        if(n==1)
-        return nums[0];
-
-        for( int i=0; i<= n-1; i++)
+        int xor= 0;
+        for(int i=0; i<n;i++)
         {
-            if(i==0)
-            {
-                if(nums[i]!= nums[i+1])
-                {
-                    return nums[i];
-                }
-            }
-            else if(i==n-1)
-            {
-                if(nums[i-1] != nums[i])
-                {
-                    return nums[i];
-                }
-            }
-            else if( nums[i]!= nums[i-1]  && nums[i] != nums[i+1])
-            return nums[i];
+            xor^= nums[i];
         }
-        return -1;        
+        return xor;
     }
 }
