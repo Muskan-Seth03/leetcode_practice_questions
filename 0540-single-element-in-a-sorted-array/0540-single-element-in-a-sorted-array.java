5cd659339f04b34a3ba4e505d89ae32e1a3cdda3
Muskan Seth <muskanseth003@gmail.com>
2025-07-20 12:21:09 +0530

    Time: 0 ms (100%), Space: 50.2 MB (80.14%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,11 +1,34 @@
-class Solution {  // brute force approach-2   XOR   TC: O(n)  SC: O(1) 
+class Solution {  //binary search
     public int singleNonDuplicate(int[] nums) {
         int n= nums.length;
-        int xor= 0;
-        for(int i=0; i<n;i++)
+        int low= 0;
+        int high= n-1;
+        while(low <= high)
         {
-            xor^= nums[i];
+            // edge case
+            if(n==1)
+            return nums[0];
+            if(nums[0]!= nums[1])
+            return nums[0];
+            if( nums[n-1] != nums[n-2])
+            return nums[n-1];
+
+            int mid = low + (high- low)/2;
+            // Check if mid is the single element
+            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
+                return nums[mid];
+            }
+            //elimination
+        //if match is of (even, odd) type then it is sure that single elment is in right half. 
+            if((mid % 2 == 0 && nums[mid] == nums[mid+1]) || (mid % 2 ==1 && nums[mid] == nums[ mid-1] ))
+            {
+                low= mid+1;
+            }
+            else
+            {
+                high= mid-1;
+            }
         }
-        return xor;
+        return -1;
     }
 }
