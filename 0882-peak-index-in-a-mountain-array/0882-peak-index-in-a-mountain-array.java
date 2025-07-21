6c282a99c7de264ea3abbb04285bff09a0ab19d1
Muskan Seth <muskanseth003@gmail.com>
2025-07-21 11:20:50 +0530

    Time: 0 ms (100%), Space: 56.5 MB (34.56%) - LeetHub

---
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
@@ -0,0 +1,38 @@
+class Solution { // TC:O(log n) SC:O(1)
+    public int peakIndexInMountainArray(int[] arr) {
+        int n= arr.length;
+        // edge case
+        if(n==1)
+        return 0;
+        if(arr[0]> arr[1])
+        {
+            return 0;
+        }
+        if(arr[n-1] > arr[n-2])
+        {
+            return n-1;
+        }
+        
+        int low=1;
+        int high= n-2;
+        while(low <= high)
+        {
+            int mid= low + (high - low)/2;
+            // check if arr[mid] is the peak element 
+            if(arr[mid]> arr[mid-1] && arr[mid] > arr[mid+1])
+            {
+                return mid;
+            }
+            // else check arr[mid] is lesser than left el or right el 
+            if( arr[mid] > arr[mid-1])
+            {
+                // eliminate left half
+                low= mid+1;
+            }
+            else
+            high = mid-1;
+        }
+        return -1;        
+    }
+}
+      
