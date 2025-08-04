a22f4521d898304600f34bfff43b6e936329ef99
Muskan Seth <muskanseth003@gmail.com>
2025-08-04 11:28:28 +0530

    Time: 3 ms (92.52%), Space: 44.2 MB (40.24%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,34 +1,24 @@
-//brute force   TC: O(2N)  SC: O(N) + O(N)
+//monotonic stack  TC: O(2N)  SC: O(N) + O(N)
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int n= nums1.length;
-        int res[]= new int[n];
-
-        for(int i=0; i< n; i++)
+        int n= nums2.length;
+        int res[]= new int[nums1.length];
+        Stack<Integer>s=new Stack<>();
+        Map<Integer, Integer> ngeMap= new HashMap<>();
+        for(int i=n-1; i>=0; i--)
         {
-            int val= nums1[i];
-
-            int index=0;
-            for(int j=0; j< nums2.length; j++)
+            while(!s.isEmpty()  && s.peek() <= nums2[i])
             {
-                if( nums2[j]== val)
-                {
-                    index= j;
-                    break;
-                }
+                s.pop();
             }
-
-            int nextGreater=-1;
-            for(int k = index + 1; k < nums2.length; k++)
-            {
-                if( nums2[k]> val)
-                {
-                    nextGreater = nums2[k];
-                    break; 
-                }
-            }
-            res[i] = nextGreater;
+            ngeMap.put(nums2[i], s.isEmpty() ? -1: s.peek());
+            s.push(nums2[i]);
         }
-        return res;        
+        for(int i=0; i<nums1.length; i++)
+        {
+            res[i]= ngeMap.get(nums1[i]);
+        }
+        return res;           
+           
     }
 }
