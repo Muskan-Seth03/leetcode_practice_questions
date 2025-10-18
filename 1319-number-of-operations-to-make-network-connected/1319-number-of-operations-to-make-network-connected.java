5eb07f68a4475d67293c42c76021f6161292e9b5
Muskan Seth <muskanseth003@gmail.com>
2025-10-18 20:09:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,86 +1,78 @@
-// DSU  TC: O(4alpha) SC:O()
-import java.util.*;
-
-class DisjointSet {
-    List<Integer> rank = new ArrayList<>();
-    List<Integer> parent = new ArrayList<>();
-    List<Integer> size = new ArrayList<>();
-    
-    public DisjointSet(int n) {
-        for (int i = 0; i < n; i++) {
-            rank.add(0);
-            parent.add(i);
-            size.add(1);
-        }
-    }
-
-    public int findUParent(int node) {
-        if (node == parent.get(node))
-            return node;
-        int ulp = findUParent(parent.get(node));
-        parent.set(node, ulp); // path compression
-        return parent.get(node);
-    }
-
-    public void unionByRank(int u, int v) {
-        int ulp_u = findUParent(u);
-        int ulp_v = findUParent(v);
-        if (ulp_u == ulp_v) return;
-
-        if (rank.get(ulp_u) < rank.get(ulp_v)) {
-            parent.set(ulp_u, ulp_v);
-        } else if (rank.get(ulp_v) < rank.get(ulp_u)) {
-            parent.set(ulp_v, ulp_u);
-        } else {
-            parent.set(ulp_v, ulp_u);
-            rank.set(ulp_u, rank.get(ulp_u) + 1);
-        }
-    }
-
-    public void unionBySize(int u, int v) {
-        int ulp_u = findUParent(u);
-        int ulp_v = findUParent(v);
-        if (ulp_u == ulp_v) return;
-
-        if (size.get(ulp_u) < size.get(ulp_v)) {
-            parent.set(ulp_u, ulp_v);
-            size.set(ulp_v, size.get(ulp_v) + size.get(ulp_u));
-        } else {
-            parent.set(ulp_v, ulp_u);
-            size.set(ulp_u, size.get(ulp_v) + size.get(ulp_u));
-        }
-    }
-}
-
+// TC: O(n) SC: O(n)
 class Solution {
-    public int makeConnected(int n, int[][] connections) {
-        DisjointSet ds= new DisjointSet(n);
-        int extraEdge=0;
-        for (int i = 0; i < connections.length; i++)
+    public int find(int i, int parent[]){
+        if(parent[i] == i)
+        return i;
+        
+        return parent[i] = find(parent[i], parent);
+    }
+    public void union(int u, int v, int []parent, int[] rank){
+       
+        int u_par= find(u, parent);
+        int v_par= find(v, parent);
+        
+        if(u_par == v_par)
+        return ;
+        
+        if(rank[u_par] >  rank[v_par])
         {
-            int u = connections[i][0];
-            int v = connections[i][1];
-            
-            if (ds.findUParent(u) == ds.findUParent(v)) 
-            {
-                extraEdge++;
-            }
-            else
-            ds.unionByRank(u,v);
+            parent[v_par]= u_par;
         }
-            
-            int countC=0;
-            for(int i=0; i<n;i++)
+        else if (rank[v_par] > rank[u_par])
+        {
+            parent[u_par]= v_par;
+        }
+        else
+        {
+            parent[u_par]= v_par;
+            rank[v_par] += 1;
+        }
+    }
+    public int makeConnected(int n, int[][] connections) {
+        int parent[] = new int[n];
+        int rank[]= new int[n];
+        
+        for(int i=0; i<n; i++)
+        {
+            parent[i]= i;
+        }
+
+        int freeEdge=0;
+        int []visi= new int[n];
+
+        for(int[] connection: connections)
+        {
+            int u= connection[0];
+            int v= connection[1];
+
+            int u_par= find(u, parent);
+            int v_par= find(v, parent);
+
+            if(u_par == v_par)
             {
-                if(ds.parent.get(i)==i)
-                {
-                    countC++;
-                }
+                // no need to use that edge ... free edge
+                freeEdge++;
             }
-            int ans=countC - 1;
-            if(extraEdge>=ans)
-            return ans;
             else
-            return -1;        
+            {
+                visi[u]=1;
+                visi[v]=1;
+                union(u, v, parent, rank);
+            }
+        }
+        int isolatedNode=0;
+        for(int i=0; i<n; i++)
+        {
+            if(visi[i] == 0)
+            {
+                isolatedNode++;
+            }
+        }
+
+        if(freeEdge >= isolatedNode)
+        return isolatedNode;
+        else
+        return -1;
+       
     }
 }
