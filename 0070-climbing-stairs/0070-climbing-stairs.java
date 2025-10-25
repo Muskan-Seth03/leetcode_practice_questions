dc5ba603c657d9b996101bc4f65daa9594f9d16f
Muskan Seth <muskanseth003@gmail.com>
2025-10-26 00:42:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,17 +1,15 @@
 class Solution {
+    public int solve(int n)
+    {
+        if(n<0)
+        return 0;
+        if(n==0)
+        return 1;
+
+        return solve(n-1) + solve(n-2);
+    }
     public int climbStairs(int n) {
-        int[] dp= new int[n+1];
-        dp[0]=1;
-        dp[1]=1;
-
-        for(int i=2; i<=n; i++)
-        {
-            dp[i]= dp[i-1] +dp[i-2];
-
-        }
-        return dp[n];
-
+        return solve(n);
+        
     }
 }
-
-  
