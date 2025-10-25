f8bc9a71dec4d62ab9a45d03957713f5d325990d
Muskan Seth <muskanseth003@gmail.com>
2025-10-26 00:50:44 +0530

    Time: 0 ms (100%), Space: 40.4 MB (52.52%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,24 +1,21 @@
-// memoization
+
 class Solution {
-    public int solve(int n, int[]dp)
-    {
-        if(n<0)
-        return 0;
-
-        if(dp[n]!=-1)
-        return dp[n];
-
-        if(n==0)
-        return 1;
-       
-
-        return dp[n]= solve(n-1,dp) + solve(n-2, dp);
-    }
     public int climbStairs(int n) {
         int[]dp =new int[n+1];
-        Arrays.fill(dp, -1);
+        if(n==1)
+        return 1;
 
-        return solve(n, dp);
+        if( n==2)
+        return 2;
         
+ 
+        dp[1]= 1;
+        dp[2]=2;
+
+        for(int i=3; i<=n; i++)
+        {
+            dp[i]= dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 }
