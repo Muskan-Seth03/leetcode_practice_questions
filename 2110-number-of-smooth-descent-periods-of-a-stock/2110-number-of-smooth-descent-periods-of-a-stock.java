6313d706cb8c1e32fb27f5d27add4e060604b76e
Muskan Seth <muskanseth003@gmail.com>
2025-12-15 23:46:38 +0530

    Time: 2 ms (100%), Space: 84.1 MB (42.96%) - LeetHub

---
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.java
@@ -0,0 +1,19 @@
+// TC: O(n) SC: O(1)
+class Solution {
+    public long getDescentPeriods(int[] prices) {
+        int n= prices.length;
+        long res= 1;
+        long count= 1;
+
+        for(int i=1; i<n; i++){
+            if(prices[i-1] - prices[i] == 1){
+                count++;
+            }
+            else{
+                count = 1;
+            }
+            res +=  count;
+        }
+        return res;
+    }
+}
