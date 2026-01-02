4e000e02d083d680423779c79cd0d0df70ecd1d4
Muskan Seth <muskanseth003@gmail.com>
2026-01-02 21:18:51 +0530

    Time: 0 ms (100%), Space: 47.9 MB (6.11%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -1,19 +1,17 @@
-// Approach-1
-// TC: O(n) SC: O(n)
+// Approach-2
+// repeated element will only be found at i-1 th and i-2 th index from ith index
+// TC: O(n) SC: O(1)
 class Solution {
     public int repeatedNTimes(int[] nums) {
         int m= nums.length;
         int n= m/2;
-        Map<Integer, Integer> map= new HashMap<>();
-
-        for(int num: nums)
+        for(int i= 2; i<m; i++)
         {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-            if(map.get(num) > 1)
+            if(nums[i] == nums[i-1] || nums[i] == nums[i-2])
             {
-                return num;
+                return nums[i];
             }
         }
-        return 0;
+        return nums[nums.length -1];
     }
 }
