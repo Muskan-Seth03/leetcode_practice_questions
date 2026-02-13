573cb497fd2b81fc810f1b6c32bf9fb8da10f790
Muskan Seth <muskanseth003@gmail.com>
2026-02-13 10:27:48 +0530

    Time: 4 ms (99.49%), Space: 48 MB (26.07%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,36 +1,36 @@
-// TC: O(n)  SC:O(1)
+// TC: O(n)   SC: O(1)
 class Solution {
-    public boolean isPalindrome(String temp, int l, int r)
+    public boolean isPalindrome(String s, int l, int r)
     {
-        while(l<r)
+        while(l < r)
         {
-            if(temp.charAt(l++) != temp.charAt(r--))
-            {
-                return false;
-            }
+            if(s.charAt(l) != s.charAt(r))
+            return false;
+
+            l++;
+            r--;
         }
         return true;
     }
     public boolean validPalindrome(String s) {
-        int l=0;
-        int r= s.length() -1;
+        int n= s.length();
 
-        if(isPalindrome(s, l, r))
-        return true;
-
-  
-        while(l<r)   
+        int l =0;
+        int r= s.length() - 1;
+        
+        while(l < r)
         {
             if(s.charAt(l) == s.charAt(r))
             {
                 l++;
                 r--;
             }
-            else
-            {
-                return isPalindrome(s, l+1, r) || isPalindrome(s, l, r-1);
+            else {
+                // try skipping either side
+                return isPalindrome(s, l+1, r) ||
+                       isPalindrome(s, l, r-1);
             }
         }
-        return false;
+        return true;
     }
 }
