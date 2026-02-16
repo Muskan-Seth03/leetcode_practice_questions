10dc5eec79634f0f745f058f5a8605fa67b8e020
Muskan Seth <muskanseth003@gmail.com>
2026-02-16 11:10:17 +0530

    Time: 5 ms (59.05%), Space: 101.1 MB (69.92%) - LeetHub

---
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -0,0 +1,67 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+// middle node + reverse list + store the maxSum 
+// TC: O(n)    SC: O(1)
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null)
+        return head;
+        
+        ListNode prev= null;
+        ListNode curr= head;
+        while(curr != null)
+        {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev= curr;
+            curr= temp;
+        }
+        return prev;
+    }
+    public int pairSum(ListNode head) {
+        // find the middle node 
+        ListNode slow= head;
+        ListNode fast= head;
+        while(fast != null && fast.next != null)
+        {
+            slow= slow.next;
+            fast= fast.next.next;
+        }
+        // break the linked list into 2 half and reverse the second half 
+        ListNode head2 = null;
+        if(fast != null)
+        {
+            head2= slow.next;
+            // slow.next = null;
+        }
+        else if(fast == null)
+        {
+            head2= slow;
+        }
+        ListNode newHead = reverseList(head2);
+        ListNode temp2= newHead;
+
+        ListNode temp1 = head;
+        
+        int maxSum= 0;
+        while(temp1 != slow && temp2 != null)
+        {
+            int sum = 0;
+
+            sum += temp1.val + temp2.val;
+            maxSum = Math.max(sum, maxSum); 
+            
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+        }
+        return maxSum;      
+    }
+}
