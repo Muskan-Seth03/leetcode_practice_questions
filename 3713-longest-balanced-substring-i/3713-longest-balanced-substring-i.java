9b2af7317052d8eca700d89f9a1befcd4bd0eb9f
Muskan Seth <muskanseth003@gmail.com>
2026-02-23 10:33:35 +0530

    Time: 743 ms (8.84%), Space: 46.9 MB (48.22%) - LeetHub

--- 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.java
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.java
@@ -1,38 +1,40 @@
+// 2 pointer approach
+// TC: O(n^2)  SC: O(n)    we can use hashmap or freq array
 class Solution {
-    public int longestBalanced(String s) {
-        int n = s.length();
-
-        // Transform char -> int
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++) 
-            a[i] = s.charAt(i) - 'a';
-
-        int result = 0;
-        for (int l = 0; l < n; l++) {
-            // Early exit, can't be bigger
-            if (n - l <= result) 
-                break;
-
-            int[] cnt = new int[26]; // Counts of every char
-            int uniq = 0, maxfreq = 0; // Number of uniq chars and maximum frequency
-            for (int r = l; r < n; r++) {
-                int i = a[r];
-
-                // There was no this char before => one more uniq
-                if (cnt[i] == 0) 
-                    uniq++;
-
-                cnt[i]++;
-                // Update max frequency
-                if (cnt[i] > maxfreq) 
-                    maxfreq = cnt[i];
-
-                // Check if all uniq chars have maxfreq frequency then update the result
-                int cur = r - l + 1;
-                if (uniq * maxfreq == cur && cur > result)
-                    result = cur;
+    private boolean isBalanced(Map<Character, Integer> map)
+    {
+        int temp = -1;
+        for(Map.Entry<Character, Integer> entry : map.entrySet())
+        {
+            if(temp == -1)
+            temp = entry.getValue();    // assign first freq
+            else
+            {
+                if(entry.getValue() != temp)
+                {
+                    return false;
+                }
             }
         }
-        return result;
+        return true;
+    }
+    public int longestBalanced(String s) {
+        int n = s.length();
+        int maxLen= 0;
+
+        for(int i=0; i<n; i++)
+        {
+            Map<Character, Integer> map = new HashMap<>();
+            for(int j= i; j<n; j++)
+            {
+                map.put(s.charAt(j), map.getOrDefault(s.charAt(j), 0) + 1);
+            
+                if(isBalanced(map))
+                {
+                    maxLen = Math.max(maxLen, j - i + 1);
+                }
+            }
+        }
+        return maxLen;
     }
 }
