308a4610c317bda2353a96a344e64913d36297ad
Muskan Seth <muskanseth003@gmail.com>
2026-02-27 12:28:26 +0530

    Time: 3 ms (39.6%), Space: 43.3 MB (54.81%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,16 +1,16 @@
-// TC: O(n)  SC: O(n)
+// TC: O(n)  SC: O(1)
 class Solution {
     public char findTheDifference(String s, String t) {
         if(s.length() == 0)
         return t.charAt(0);
         
-        int xor1 = (int) s.charAt(0);
-        for(int i=1; i<s.length(); i++)
+        int xor1 = 0;
+        for(int i=0; i<s.length(); i++)
         {
             xor1 ^= s.charAt(i) - 'a';
         }
-        int xor2 = (int) t.charAt(0);
-        for(int i=1; i<t.length(); i++)
+        int xor2 = 0;
+        for(int i=0; i<t.length(); i++)
         {
             xor2 ^= t.charAt(i) - 'a';
         }
