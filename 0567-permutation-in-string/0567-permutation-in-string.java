1b36d1ee5b7261ce039e498b28dc1bd1c4b61413
Muskan Seth <muskanseth003@gmail.com>
2026-03-09 13:39:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,54 +1,34 @@
-// sliding window  
-// TC: O(n)  SC: O(k)    k-> length of pattern
+// sorting approach
+// TC: O((n log n) * (n - m))   SC: O(m)
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int m = s1.length();
-        
-        Map<Character, Integer> map = new HashMap<>();
-        for(char c : s1.toCharArray())
-            map.put(c, map.getOrDefault(c, 0) + 1);
-        
-        // sliding window approach
-        int i = 0;
-        int j = 0;
-
+        int m= s1.length();
         int n= s2.length();
-        int ans = 0;
-        while(j < n)
-        {
-            if(map.containsKey(s2.charAt(j)))
-                map.put(s2.charAt(j), map.get(s2.charAt(j)) - 1);
-            
-            int size = j - i + 1;
 
-            if(size < m)
+        if(m > n)
+        {
+            return false;
+        }
+        char[] arr = s1.toCharArray();
+        Arrays.sort(arr);
+
+        String sorted = new String(arr);
+
+        for(int i=0; i <= n - m; i++)
+        {
+            String substring = s2.substring(i, i + m);
+
+            char[] arr1 = substring.toCharArray();
+            Arrays.sort(arr1);
+
+            String sortedSubstring = new String(arr1);
+
+
+            if(sortedSubstring.equals(s1))
             {
-                j++;
-            }
-            else if(size == m)
-            {
-                boolean flag = true;
-                // check in map if all value sare zero
-                for(Integer val : map.values())
-                {
-                    if(val != 0)
-                    {
-                        flag = false;
-                        break;
-                    }
-                }
-                if(flag)
-                {
-                    ans += 1;
-                }
-                if(map.containsKey(s2.charAt(i)))
-                {
-                    map.put(s2.charAt(i), map.get(s2.charAt(i)) + 1);
-                }
-                i++;
-                j++;
+                return true;
             }
         }
-        return ans > 0 ? true : false;
+        return false;
     }
 }
