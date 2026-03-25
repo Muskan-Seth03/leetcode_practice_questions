c9fbdf77020796f24f3dda6c4bd799c95a775fbf
Muskan Seth <muskanseth003@gmail.com>
2026-03-25 12:36:27 +0530

    Time: 0 ms (100%), Space: 46.7 MB (80.78%) - LeetHub

---
+++ 0867-transpose-matrix/0867-transpose-matrix.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[][] transpose(int[][] mat) {
+        int m= mat.length;
+        int n= mat[0].length;
+
+        int[][] result = new int[n][m];
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                result[j][i] = mat[i][j];
+            }
+        }
+        return result;
+    }
+}
