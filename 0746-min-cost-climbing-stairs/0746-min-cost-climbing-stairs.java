bcef96afeffb0ad10376662947f0885f2e414299
Muskan Seth <muskanseth003@gmail.com>
2026-04-09 11:12:03 +0530

    Time: 1 ms (38.25%), Space: 45.1 MB (28.62%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,32 +1,23 @@
-import java.util.Arrays;
-class Solution { // recursion + memoization
+// memoization 
+class Solution {
+    public int solve(int ind, int[] cost, int[] dp)
+    {
+        if(ind >= cost.length)
+        return 0;
 
-    public int solve(int [] cost, int n, int []dp){
-        //base case
-        if(n==0)
-        return cost[0];
+        if(dp[ind] != -1)
+        return dp[ind];
 
-        if(n==1)
-        return cost[1];
+        int a = cost[ind] + solve(ind+1, cost, dp);
+        int b = cost[ind] + solve(ind+2, cost, dp);
 
-        if(dp[n]!=-1)
-        return dp[n];
-
-
-        dp[n]= (cost[n]+ Math.min(solve(cost, n-1, dp), solve(cost, n-2, dp)));
-        return dp[n];
+        return dp[ind] = Math.min(a, b);
     }
-
     public int minCostClimbingStairs(int[] cost) {
-        int n= cost.length;
+        int n = cost.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
 
-        int dp[]= new int[n+1];
-        Arrays.fill(dp,-1);
-
-        int ans= Math.min(solve(cost, n-1, dp), solve(cost, n-2, dp));
-
-        return ans;
-
-        
+        return Math.min(solve(0, cost, dp), solve(1, cost, dp));
     }
 }
