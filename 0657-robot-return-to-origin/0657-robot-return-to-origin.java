1f12d5c07b496c6de24156b0866b2be8833c9ec6
Muskan Seth <muskanseth003@gmail.com>
2026-04-24 11:19:02 +0530

    Time: 5 ms (61.47%), Space: 46.2 MB (34.01%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -1,12 +1,32 @@
 class Solution {
     public boolean judgeCircle(String moves) {
-        int x = 0, y = 0;
-        for (char c : moves.toCharArray()) {
-            if (c == 'R') x++;
-            if (c == 'L') x--;
-            if (c == 'U') y++;
-            if (c == 'D') y--;
+        int left = 0, right = 0, up = 0, down = 0;
+        for(char move : moves.toCharArray())
+        {
+            if(move == 'L')
+            {
+                left++;
+            }
+            else if(move == 'R')
+            {
+                right++;
+            }
+            else if(move == 'U')
+            {
+                up++;
+            }
+            else
+            {
+                down++;
+            }
         }
-        return x == 0 && y == 0;
+        if(Math.abs(left - right) == 0 && Math.abs(up - down) == 0)
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
     }
 }
