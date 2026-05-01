029c1320d2f3c00151e7f45a40d5697f400897f9
Muskan Seth <muskanseth003@gmail.com>
2026-05-01 12:21:48 +0530

    Time: 47 ms (36.53%), Space: 45.9 MB (82.24%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,29 +1,23 @@
-// memoization approach
+// tabulation approach
 class Solution {
-    int[][] dp;
-    public int solve(int i, int prev, int[] nums)
-    {
-        int n= nums.length;
-
-        if(i >= n)
-        return 0;
-        
-        if(dp[i][prev+1] != -1)
-        return dp[i][prev+1]; 
-
-        int skip = solve(i+1, prev, nums);
-
-        int take = (prev == -1 || nums[i] > nums[prev]) ? 1 + solve(i+1, i, nums) : 0;
-
-        return dp[i][prev+1] = Math.max(skip, take);
-    }
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        dp = new int[n+1][n+1];
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
 
-        for(int[] d: dp)
-        Arrays.fill(d, -1);
+        int maxLIS = 1;
 
-        return solve(0, -1, nums);
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<i; j++)
+            {
+                if(nums[j] < nums[i])
+                {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    maxLIS = Math.max(maxLIS, dp[i]);
+                }
+            }
+        }
+        return maxLIS;
     }
 }
