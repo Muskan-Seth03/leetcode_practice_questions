7f0751f1e5e0ff466f5bc5cf5604bad4a5ab5037
Muskan Seth <muskanseth003@gmail.com>
2026-05-03 00:32:52 +0530

    Time: 4 ms (59.28%), Space: 42.1 MB (56.33%) - LeetHub

---
+++ 0788-rotated-digits/0788-rotated-digits.java
@@ -0,0 +1,36 @@
+//Approach-1 (Brute Force SImulation)
+//T.C : O(n)
+//S.C : O(n* log10(num))
+class Solution {
+
+    private boolean isGood(int num) {
+        boolean changed = false;
+
+        while (num > 0) {
+            int digit = num % 10;
+
+            if (digit == 3 || digit == 4 || digit == 7) return false;
+            if (digit == 2 || digit == 5 || digit == 6 || digit == 9) changed = true;
+
+            num /= 10;
+        }
+        return changed;
+    }
+
+    public int rotatedDigits(int n) {
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (isGood(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+
+
+//Approach-2 (Recursion Memo)
+//T.C : O(n)
+//S.C : O(n)
