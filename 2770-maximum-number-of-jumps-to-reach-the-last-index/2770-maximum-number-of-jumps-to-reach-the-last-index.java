2b49eb172e7f7878f47fe7810681b60f702d910b
Muskan Seth <muskanseth003@gmail.com>
2026-05-10 12:58:23 +0530

    Time: 21 ms (23.21%), Space: 46.7 MB (90.48%) - LeetHub

--- 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.java
+++ 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.java
@@ -1,11 +1,14 @@
-// recursive approach
+// recursion + memoization approach
 class Solution {
-    public int solve(int i, int[] nums, int target)
+    public int solve(int i, int[] nums, int target, int[] dp)
     {
         int n = nums.length;
         // base case
         if(i == n-1)
         return 0;
+
+        if(dp[i] != Integer.MIN_VALUE)
+        return dp[i];
 
         int res = Integer.MIN_VALUE;
 
@@ -13,15 +16,19 @@ class Solution {
         {
             if(Math.abs(nums[i] - nums[j]) <= target)
             {
-                int temp = 1 + solve(j, nums, target);
+                int temp = 1 + solve(j, nums, target, dp);
                 res = Math.max(res, temp); 
             }
         }
-        return res;
+        return dp[i] = res;
     }
     public int maximumJumps(int[] nums, int target) {
         int n = nums.length;
-        int result = solve(0, nums, target);
+        int[] dp = new int[n];
+
+        Arrays.fill(dp, Integer.MIN_VALUE);
+
+        int result = solve(0, nums, target, dp);
         return result < 0 ? -1 : result; 
     }
 }
