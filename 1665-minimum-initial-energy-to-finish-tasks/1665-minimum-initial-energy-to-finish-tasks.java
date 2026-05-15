2ac306bb71196bcbe99302497d42e50061f56354
Muskan Seth <muskanseth003@gmail.com>
2026-05-15 08:36:15 +0530

    Time: 30 ms (14.76%), Space: 113.9 MB (64.6%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.java
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.java
@@ -1,18 +1,43 @@
+//Approach (Binary search on answer + greedy sorting)
+//T.C : O(n*logn)
+//S.C : O(1)
 class Solution {
-    public int minimumEffort(int[][] tasks) {
-        Arrays.sort(tasks, (a, b) -> (b[1] - b[0]) - (a[1] - a[0]));
-
-        int curr = 0;
-        int ans  = 0;
-        for (int[] task : tasks) {
+    public boolean isPossible(int[][] tasks, int mid) {
+        for(int[] task : tasks) {
             int actual  = task[0];
             int minimum = task[1];
-            if (curr < minimum) {
-                ans  += (minimum - curr);
-                curr  = minimum;
+            if(minimum > mid) {
+                return false;
             }
-            curr -= actual;
+            mid -= actual;
         }
-        return ans;
+        return true;
+    }
+    public int minimumEffort(int[][] tasks) {
+
+        int n = tasks.length;
+
+        int l = 0;
+        int r = (int)1e9;
+
+        int result = Integer.MAX_VALUE;
+
+        Arrays.sort(tasks, (task1, task2) -> {
+            int diff1 = task1[1] - task1[0];
+            int diff2 = task2[1] - task2[0];
+
+            return diff2 - diff1;
+        });
+
+        while(l <= r) {
+            int mid = l + (r-l)/2;
+            if(isPossible(tasks, mid)) {
+                result = mid;
+                r = mid-1;
+            } else {
+                l = mid+1;
+            }
+        }
+        return result;
     }
 }
