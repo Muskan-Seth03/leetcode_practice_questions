f7e45047a2804be670f52e329f269c65be57100b
Muskan Seth <muskanseth003@gmail.com>
2026-05-15 13:35:13 +0530

    Time: 14 ms (49%), Space: 41.9 MB (71.43%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -3,21 +3,22 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int low=1; int high=n;
-        while(low<=high)
+        int l = 0;
+        int r = n;
+        int ind = -1;
+        while(l <= r)
         {
-            int mid = low + (high - low) / 2; // Avoid potential overflow
+            int mid = l + (r-l)/2;
             if(isBadVersion(mid))
             {
-               high=mid-1;
+                ind = mid;
+                r = mid - 1;
             }
             else
             {
-                low= mid+1;
-
+                l = mid+1;
             }
         }
-        return low;
-
+        return ind;
     }
 }
