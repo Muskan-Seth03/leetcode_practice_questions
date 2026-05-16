c32771052de5cccb747987b813710568e7ee01bf
Muskan Seth <muskanseth003@gmail.com>
2026-05-16 06:48:49 +0530

    Time: 0 ms (100%), Space: 52.4 MB (96.77%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,34 +1,40 @@
-class Solution {  //binary search
+// TC: O(log n)
+class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int n= nums.length;
-        int low= 0;
-        int high= n-1;
-        while(low <= high)
-        {
-            // edge case
-            if(n==1)
-            return nums[0];
-            if(nums[0]!= nums[1])
-            return nums[0];
-            if( nums[n-1] != nums[n-2])
-            return nums[n-1];
+        int n = nums.length;
+        int l = 0;
+        int r = n-1;
 
-            int mid = low + (high- low)/2;
-            // Check if mid is the single element
-            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
-                return nums[mid];
-            }
-            //elimination
-        //if match is of (even, odd) type then it is sure that single elment is in right half. 
-            if((mid % 2 == 0 && nums[mid] == nums[mid+1]) || (mid % 2 ==1 && nums[mid] == nums[ mid-1] ))
+        while(l < r)
+        {
+            int mid = l +(r-l)/2;
+            boolean isEven = (r - mid) % 2 == 0;
+
+            if(nums[mid] == nums[mid+1])
             {
-                low= mid+1;
+                // check no of el to the right of mid
+                if(isEven)
+                {
+                    // go right
+                    l = mid + 2;
+                }
+                else
+                {
+                    r = mid - 1;
+                } 
             }
             else
             {
-                high= mid-1;
+                if(isEven)
+                {
+                    r = mid;
+                }
+                else
+                {
+                    l = mid+1;
+                }
             }
         }
-        return -1;
+        return nums[r];
     }
 }
