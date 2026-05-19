841b2d0561fe307a725059e69bd9a89c7a8ea0bd
Muskan Seth <muskanseth003@gmail.com>
2026-05-19 17:45:56 +0530

    Time: 59 ms (24.19%), Space: 45.9 MB (98.19%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,7 +1,7 @@
-// recursion + memoization
+// tabulation approach
 class Solution {
     int n;
-    int[][] dp;
+    int[] dp;
     public boolean isPredecessor(String a, String b)
     {
         if(a.length() - b.length() != 1 || b.length() >= a.length())
@@ -29,33 +29,27 @@ class Solution {
             return false;
         }
     }
-    public int LIS(String[] words, int currInd, int prevInd)
-    {
-        if(currInd >= words.length)
-        return 0;
-
-        if(dp[currInd][prevInd+1] != -1)
-        return dp[currInd][prevInd+1];
-
-        int skip = LIS(words, currInd + 1, prevInd);
-
-        int take = 0;
-        if(prevInd == -1 || isPredecessor(words[currInd], words[prevInd]))
-        {
-            take = 1 + LIS(words, currInd + 1, currInd);
-        }
-        return dp[currInd][prevInd+1] = Math.max(take, skip);
-    }
     public int longestStrChain(String[] words) {
         n = words.length;
-        dp = new int[n+1][n+1];
-
-        for(int[] d: dp)
-        {
-            Arrays.fill(d, -1);
-        }
+        dp = new int[n+1];
+        
+        Arrays.fill(dp, 1);
+        
         Arrays.sort(words, (a, b) -> Integer.compare(a.length(), b.length()));
 
-        return LIS(words, 0, -1);
+        int maxLIS = 1;
+
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<i; j++)
+            {
+                if(isPredecessor(words[i], words[j]))
+                {
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                    maxLIS = Math.max(maxLIS, dp[i]);
+                }
+            }
+        }
+        return maxLIS;
     }
 }
