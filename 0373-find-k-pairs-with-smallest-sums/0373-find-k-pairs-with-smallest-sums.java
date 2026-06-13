827e04f641036a94fa7e4dfa99c5863692888bd1
Muskan Seth <muskanseth003@gmail.com>
2026-06-13 13:33:41 +0530

    Time: 88 ms (10.89%), Space: 145.1 MB (5.3%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -1,5 +1,5 @@
-// TC: O(m * n * log k)  SC: O(k)
-// standard heap problem (brute force)
+// TC: O(k log k)  SC: O(k)
+// optimal approach (PQ + set)
 class Pair implements Comparable<Pair>{
     int sum;
     int[] arr;
@@ -17,51 +17,59 @@ class Pair implements Comparable<Pair>{
 }
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>(Collections.reverseOrder());
-        
         int m = nums1.length;
         int n = nums2.length;
-        int sum = 0;
 
-        for(int i=0; i<m; i++)
-        {
-            for(int j=0; j<n; j++)
-            {
-                sum = nums1[i] + nums2[j];
-
-                if(pq.size() < k)
-                {
-                    pq.offer(new Pair(sum, new int[]{i, j}));
-                }
-                else if(pq.peek().sum > sum)
-                {
-                    pq.poll();
-                    pq.offer(new Pair(sum, new int[]{i, j}));
-                }
-                else
-                {
-                    break;
-                }
-            }
-        }
         List<List<Integer>> res = new ArrayList<>();
 
+        if(m == 0 || n == 0 || k == 0)
+        {
+            return res;
+        }
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        
+        Set<String> visi = new HashSet<>();
+
+        int sum = nums1[0] + nums2[0];
+
+        pq.offer(new Pair(sum, new int[]{0, 0}));
+        visi.add("0,0");
        
-        while(!pq.isEmpty())
+        while(k-- > 0 && !pq.isEmpty())
         {
             List<Integer> list = new ArrayList<>();
 
             Pair temp = pq.poll();
             int[] curr = temp.arr;
-            int i= curr[0];
-            int j= curr[1];
-            
+            int i = curr[0];
+            int j = curr[1];
+
             list.add(nums1[i]);
             list.add(nums2[j]);
 
             res.add(list);
+
+            // out of bound check + duplicate check   for (i+1, j)
+            String down = (i + 1) + "," + j;
+            
+            if(i+1 < m && !visi.contains(down))
+            {
+                pq.offer(new Pair(nums1[i+1] + nums2[j], new int[]{i+1, j}));
+
+                visi.add(down);
+            }
+
+            // out of bound check + duplicate check   for (i, j+1)
+            String right = i + "," + (j + 1);
+            
+            if(j+1 < n && !visi.contains(right))
+            {
+                pq.offer(new Pair(nums1[i] + nums2[j+1], new int[]{i, j+1}));
+                
+                visi.add(right);
+            }
         }
-        Collections.reverse(res);
         return res;
     }
 }
