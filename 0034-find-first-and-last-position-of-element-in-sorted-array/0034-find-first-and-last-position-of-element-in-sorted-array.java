9e40e81a0ea22661bb86d16c3a4fa6cfb3204a7e
Muskan Seth <muskanseth003@gmail.com>
2026-07-13 23:26:59 +0530

    Time: 1 ms (17.46%), Space: 48.1 MB (51.19%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,47 +1,33 @@
+// TC: O(n)    SC: O(n)
 class Solution {
-    public int binarySearch(int low, int high, int[] nums, int target)
-    {
-        int ind = -1;
-        while(low <= high)
-        {
-            int mid = low + (high - low)/2;
-            if(nums[mid] == target)
-            {
-                ind = mid;
-                break;
-            }            
-            else if(nums[mid] < target)
-            {
-                low = mid + 1;
-            }
-            else
-            {
-                high = mid-1;
-            }
-        }
-        return ind;
-    }
     public int[] searchRange(int[] nums, int target) {
         int n = nums.length;
-        int pivot = binarySearch(0, n-1, nums, target);
 
-        if(pivot == -1)
+        int less= 0;
+        int equal =0;
+
+        boolean flag = false;
+        for(int num: nums)
+        {
+            if(num == target)
+            {
+                equal++;
+                flag = true;
+            }
+            else if(num < target)
+            {
+                less++;
+            }
+        }
+        if(!flag) 
         return new int[]{-1, -1};
 
-        int index = pivot;
+        int[] res = new int[2];
+        int k = 0;
 
-        while(index > 0 && nums[index] == nums[index-1])
-        {
-            index--;
-        }       
-        int ind1= index;
-
-        index = pivot;
-        while(index < n-1 && nums[index] == nums[index+1])
-        {
-            index++;
-        } 
-        int ind2 = index;
-        return new int[]{ind1, ind2};
+        res[0] = less;
+        res[1] = less + equal - 1;
+        
+        return res;       
     }
 }
