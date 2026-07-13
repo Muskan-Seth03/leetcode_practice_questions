a12180291e3164602f5896382a8280eefaa6fc5d
Muskan Seth <muskanseth003@gmail.com>
2026-07-13 23:00:13 +0530

    Time: 0 ms (100%), Space: 44.7 MB (82.14%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
@@ -1,19 +1,31 @@
-// TC: O(n log n) after sorting the array
-// SC: O(n)
+// TC: O(n) without sorting the array
+// SC: O(1)
 class Solution {
     public List<Integer> targetIndices(int[] nums, int target) {
         List<Integer> list = new ArrayList<>();
 
         int n= nums.length;
-        Arrays.sort(nums);
 
+        int less = 0;
+        int equal = 0;
+        // calculate count of el less than target and el equal to target
         for(int i=0; i<nums.length; i++)
         {
             if(nums[i] == target)
             {
-                list.add(i);
+                equal++;
+            }
+            else if(nums[i] < target)
+            {
+                less++;
             }
         }
+
+        for(int i = less; i< less+equal; i++)
+        {
+            list.add(i);
+        }
+
         return list;
     }
 }
