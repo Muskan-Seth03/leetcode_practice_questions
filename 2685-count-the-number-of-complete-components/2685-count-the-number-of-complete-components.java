8154b04bdefb23f54492ba06665fa0de9d860d50
Muskan Seth <muskanseth003@gmail.com>
2026-07-14 12:06:15 +0530

    Time: 13 ms (28.49%), Space: 47.8 MB (21.71%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.java
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.java
@@ -1,54 +1,61 @@
-// DFS approach
-// TC: O(V + E)  SC: O(V)
+// bfs approach 
+// TC: O(V + E)    SC: O(V + E)
 class Solution {
-    public void dfs(int i, List<List<Integer>> adjList, boolean[] visi, int[] count)
-    {   
-        visi[i] = true;
-        count[0] += 1;
-        count[1] += adjList.get(i).size();
+    public void bfs(int start, List<List<Integer>> adjList, boolean[] visi, int[] count) {
 
-        for(int j=0; j< adjList.get(i).size(); j++)
-        {
-            List<Integer> neighbour = adjList.get(i);
-            
-            if(!visi[neighbour.get(j)])
-            dfs(neighbour.get(j), adjList, visi, count);
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+        visi[start] = true;
+
+        while (!q.isEmpty()) {
+
+            int node = q.poll();
+
+            count[0]++;                       // vertices
+            count[1] += adjList.get(node).size(); // degree sum
+
+            for (int neighbour : adjList.get(node)) {
+
+                if (!visi[neighbour]) {
+                    visi[neighbour] = true;
+                    q.offer(neighbour);
+                }
+            }
         }
     }
+
     public int countCompleteComponents(int n, int[][] edges) {
-        int res = 0;
 
         List<List<Integer>> adjList = new ArrayList<>();
 
-        for(int i = 0; i<n; i++)
-        {
+        for (int i = 0; i < n; i++)
             adjList.add(new ArrayList<>());
-        }
-        for(int[] edge : edges)
-        {
-            int u = edge[0];
-            int v = edge[1];
-            adjList.get(u).add(v);
-            adjList.get(v).add(u);
-        }
-        boolean[] visi = new boolean[n];
-        Arrays.fill(visi, false);
 
-        for(int i=0; i<n; i++)
-        {
-            if(!visi[i])
-            {
+        for (int[] edge : edges) {
+            adjList.get(edge[0]).add(edge[1]);
+            adjList.get(edge[1]).add(edge[0]);
+        }
+
+        boolean[] visi = new boolean[n];
+
+        int res = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            if (!visi[i]) {
+
                 int[] count = new int[2];
 
-                dfs(i, adjList, visi, count);
+                bfs(i, adjList, visi, count);
 
-                int v = count[0];
-                int e = count[1];
+                int vertices = count[0];
+                int degreeSum = count[1];
 
-                if((v*(v-1)) == e)
-                res++;
+                if (degreeSum == vertices * (vertices - 1))
+                    res++;
             }
         }
+
         return res;
     }
 }
