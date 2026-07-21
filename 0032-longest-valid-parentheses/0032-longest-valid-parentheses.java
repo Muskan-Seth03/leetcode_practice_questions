c9ddff44ae1e938becf5edf4ebfb743d2d8f34bb
Muskan Seth <muskanseth003@gmail.com>
2026-07-21 13:03:49 +0530

    Time: 5 ms (82.07%), Space: 46.6 MB (43.12%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -1,44 +1,21 @@
+// tc: O(n)  sc: O(n)
 class Solution {
     public int longestValidParentheses(String s) {
-        Stack<Integer> st = new Stack<>();
-
-        st.push(-1);
-
-        for(int i=0; i<s.length(); i++)
-        {
-            if(s.charAt(i) == '(')
-            {
-                st.push(i);
-            }
-            else if(s.charAt(i) == ')')
-            {
-                if(!st.isEmpty() && st.peek() != -1 && s.charAt(st.peek()) == '(')
-                {
-                    st.pop();
-                }
-                else
-                {
-                    st.push(i);
-                }
-            }
-        }
-        st.push(s.length());
-
-        int[] arr = new int[st.size()];
-        int i=0;
-
-        while(!st.isEmpty())
-        {
-            arr[i++] = st.peek();
-            st.pop();
-        }
-
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
         int maxLen = 0;
-        for(int j =0; j+1 < arr.length; j++)
-        {
-            int currLen = arr[j] - arr[j+1] - 1;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
 
-            maxLen = Math.max(maxLen, currLen);
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    maxLen = Math.max(maxLen, i - stack.peek());
+                }
+            }
         }
         return maxLen;
     }
